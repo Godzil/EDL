c611d694ce766798b33e21c5c4f752366cddf6dd
SavourySnaX <savoury.snax@googlemail.com>
2014-01-27 05:38:19 +0000

    wip

---
+++ examples/2600/2600.c
@@ -0,0 +1,727 @@
+/*
+ * VIC20 implementation
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+#include <GLFW/glfw3.h>
+#include <GL/glext.h>
+
+#include <al.h>
+#include <alc.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+void AudioKill();
+void AudioInitialise();
+void UpdateAudio();
+void _AudioAddData(int channel,int16_t dacValue);
+
+uint16_t MAIN_PinGetAB();
+uint8_t MAIN_PinGetDB();
+void MAIN_PinSetDB(uint8_t);
+void MAIN_PinSetO0(uint8_t);
+uint8_t MAIN_PinGetRW();
+void MAIN_PinSet_RES(uint8_t);
+
+
+uint8_t MAIN_PinGetSYNC();	// just for debugging
+
+// Step 1. Memory
+
+unsigned char Ram[0x80];
+unsigned char Rom[0x1000];
+
+int LoadRom(unsigned char* rom,unsigned int size,const char* fname)
+{
+	unsigned int readFileSize=0;
+	FILE* inFile = fopen(fname,"rb");
+	if (!inFile || size != (readFileSize = fread(rom,1,size,inFile)))
+	{
+		printf("Failed to open rom : %s - %d/%d",fname,readFileSize,size);
+		return 1;
+	}
+	fclose(inFile);
+	return 0;
+}
+
+
+int InitialiseMemory()
+{
+	if (LoadRom(Rom,2048,"roms/volymN.bin"))
+		return 1;
+	if (LoadRom(&Rom[2048],2048,"roms/volymN.bin"))		// mirror
+		return 1;
+
+	return 0;
+}
+
+int doDebug=0;
+
+// MMAP - apparantly
+// ROM - A12 = 1
+// RAM - A12 = 0 & A7 = 1 & A9 = 0
+// TIA - A12 = 0 & A7 = 0
+// RIOT - A12 = 0 & A7 = 1 & A9 = 1
+
+//	12 11 10 9 8 7 6 5 4 3 2 1 0
+//	1  0  0  1 0 1 0 0 0 0 0 0 0
+	
+//	0x1280
+
+uint8_t GetByte(uint16_t addr)
+{
+	if (addr&0x1000)
+	{
+		printf("ROM ACCESS : %04X\n",addr);
+		return Rom[addr&0xFFF];
+	}
+	if ((addr&0x1080)==0x00)
+	{
+		printf("TIA ACCESS : %04X\n",addr);
+		return 0;
+	}
+	if ((addr&0x1280)==0x80)
+	{
+		printf("RAM ACCESS : %04X\n",addr);
+		return Ram[addr-0x80];
+	}
+	if ((addr&0x1280)==0x280)
+	{
+		printf("RIOT ACCESS : %04X\n",addr);
+		return 0;
+	}
+	return 0;
+}
+
+void SetByte(uint16_t addr,uint8_t byte)
+{
+	if (addr&0x1000)
+	{
+		printf("ROM WRITE??? : %04X,%02X\n",addr,byte);
+	}
+	if ((addr&0x1080)==0x00)
+	{
+		printf("TIA WRITE : %04X,%02X\n",addr,byte);
+	}
+	if ((addr&0x1280)==0x80)
+	{
+		printf("RAM WRITE : %04X,%02X\n",addr,byte);
+		Ram[addr-0x80]=byte;
+	}
+	if ((addr&0x1280)==0x280)
+	{
+		printf("RIOT WRITE : %04X,%02X\n",addr,byte);
+	}
+}
+
+
+int masterClock=0;
+int pixelClock=0;
+int cpuClock=0;
+
+int KeyDown(int key);
+int CheckKey(int key);
+void ClearKey(int key);
+
+extern uint8_t *MAIN_DIS_[256];
+
+extern uint8_t	MAIN_A;
+extern uint8_t	MAIN_X;
+extern uint8_t	MAIN_Y;
+extern uint16_t	MAIN_SP;
+extern uint16_t	MAIN_PC;
+extern uint8_t	MAIN_P;
+
+void DUMP_REGISTERS()
+{
+	printf("--------\n");
+	printf("FLAGS = N  V  -  B  D  I  Z  C\n");
+	printf("        %s  %s  %s  %s  %s  %s  %s  %s\n",
+			MAIN_P&0x80 ? "1" : "0",
+			MAIN_P&0x40 ? "1" : "0",
+			MAIN_P&0x20 ? "1" : "0",
+			MAIN_P&0x10 ? "1" : "0",
+			MAIN_P&0x08 ? "1" : "0",
+			MAIN_P&0x04 ? "1" : "0",
+			MAIN_P&0x02 ? "1" : "0",
+			MAIN_P&0x01 ? "1" : "0");
+	printf("A = %02X\n",MAIN_A);
+	printf("X = %02X\n",MAIN_X);
+	printf("Y = %02X\n",MAIN_Y);
+	printf("SP= %04X\n",MAIN_SP);
+	printf("--------\n");
+}
+
+const char* decodeDisasm(uint8_t *table[256],unsigned int address,int *count,int realLength)
+{
+	static char temporaryBuffer[2048];
+	char sprintBuffer[256];
+
+	uint8_t byte = GetByte(address);
+	if (byte>realLength)
+	{
+		sprintf(temporaryBuffer,"UNKNOWN OPCODE");
+		return temporaryBuffer;
+	}
+	else
+	{
+		const char* mnemonic=table[byte];
+		const char* sPtr=mnemonic;
+		char* dPtr=temporaryBuffer;
+		int counting = 0;
+		int doingDecode=0;
+
+		if (sPtr==NULL)
+		{
+			sprintf(temporaryBuffer,"UNKNOWN OPCODE");
+			return temporaryBuffer;
+		}
+
+		while (*sPtr)
+		{
+			if (!doingDecode)
+			{
+				if (*sPtr=='%')
+				{
+					doingDecode=1;
+					if (*(sPtr+1)=='$')
+						sPtr++;
+				}
+				else
+				{
+					*dPtr++=*sPtr;
+				}
+			}
+			else
+			{
+				char *tPtr=sprintBuffer;
+				int negOffs=1;
+				if (*sPtr=='-')
+				{
+					sPtr++;
+					negOffs=-1;
+				}
+				int offset=(*sPtr-'0')*negOffs;
+				sprintf(sprintBuffer,"%02X",GetByte(address+offset));
+				while (*tPtr)
+				{
+					*dPtr++=*tPtr++;
+				}
+				doingDecode=0;
+				counting++;
+			}
+			sPtr++;
+		}
+		*dPtr=0;
+		*count=counting;
+	}
+	return temporaryBuffer;
+}
+
+int Disassemble(unsigned int address,int registers)
+{
+	int a;
+	int numBytes=0;
+	const char* retVal = decodeDisasm(MAIN_DIS_,address,&numBytes,255);
+
+	if (strcmp(retVal,"UNKNOWN OPCODE")==0)
+	{
+		printf("UNKNOWN AT : %04X\n",address);
+		for (a=0;a<numBytes+1;a++)
+		{
+			printf("%02X ",GetByte(address+a));
+		}
+		printf("\n");
+		exit(-1);
+	}
+
+	if (registers)
+	{
+		DUMP_REGISTERS();
+	}
+	printf("%04X :",address);
+
+	for (a=0;a<numBytes+1;a++)
+	{
+		printf("%02X ",GetByte(address+a));
+	}
+	for (a=0;a<8-(numBytes+1);a++)
+	{
+		printf("   ");
+	}
+	printf("%s\n",retVal);
+
+	return numBytes+1;
+}
+
+int dumpInstruction=0;
+
+int g_instructionStep=0;
+int g_traceStep=0;
+
+#define HEIGHT	(262)
+#define	WIDTH	(228)
+
+#define MAX_WINDOWS		(1)
+
+#define MAIN_WINDOW		0
+
+GLFWwindow* windows[MAX_WINDOWS];
+unsigned char *videoMemory[MAX_WINDOWS];
+GLint videoTexture[MAX_WINDOWS];
+
+void ShowScreen(int windowNum,int w,int h)
+{
+	glBindTexture(GL_TEXTURE_RECTANGLE_NV, videoTexture[windowNum]);
+	
+	// glTexSubImage2D is faster when not using a texture range
+	glTexSubImage2D(GL_TEXTURE_RECTANGLE_NV, 0, 0, 0, w, h, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, videoMemory[windowNum]);
+	glBegin(GL_QUADS);
+
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex2f(-1.0f,1.0f);
+
+	glTexCoord2f(0.0f, h);
+	glVertex2f(-1.0f, -1.0f);
+
+	glTexCoord2f(w, h);
+	glVertex2f(1.0f, -1.0f);
+
+	glTexCoord2f(w, 0.0f);
+	glVertex2f(1.0f, 1.0f);
+	glEnd();
+	
+	glFlush();
+}
+
+void setupGL(int windowNum,int w, int h) 
+{
+	videoTexture[windowNum] = windowNum+1;
+	videoMemory[windowNum] = (unsigned char*)malloc(w*h*sizeof(unsigned int));
+	memset(videoMemory[windowNum],0,w*h*sizeof(unsigned int));
+	//Tell OpenGL how to convert from coordinates to pixel values
+	glViewport(0, 0, w, h);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	glClearColor(1.0f, 0.f, 1.0f, 1.0f);
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity(); 
+
+	glDisable(GL_TEXTURE_2D);
+	glEnable(GL_TEXTURE_RECTANGLE_NV);
+	glBindTexture(GL_TEXTURE_RECTANGLE_NV, videoTexture[windowNum]);
+
+	//	glTextureRangeAPPLE(GL_TEXTURE_RECTANGLE_EXT, 0, NULL);
+
+	//	glTexParameteri(GL_TEXTURE_RECTANGLE_EXT, GL_TEXTURE_STORAGE_HINT_APPLE , GL_STORAGE_CACHED_APPLE);
+	//	glPixelStorei(GL_UNPACK_CLIENT_STORAGE_APPLE, GL_TRUE);
+	glTexParameteri(GL_TEXTURE_RECTANGLE_NV, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_RECTANGLE_NV, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_RECTANGLE_NV, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_RECTANGLE_NV, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+
+	glTexImage2D(GL_TEXTURE_RECTANGLE_NV, 0, GL_RGBA, w,
+			h, 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, videoMemory[windowNum]);
+
+	glDisable(GL_DEPTH_TEST);
+}
+
+void restoreGL(int windowNum,int w, int h) 
+{
+	//Tell OpenGL how to convert from coordinates to pixel values
+	glViewport(0, 0, w, h);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	glClearColor(1.0f, 0.f, 1.0f, 1.0f);
+	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity(); 
+
+	glDisable(GL_TEXTURE_2D);
+	glEnable(GL_TEXTURE_RECTANGLE_NV);
+	glDisable(GL_DEPTH_TEST);
+}
+
+struct KeyArray
+{
+	unsigned char lastState;
+	unsigned char curState;
+	unsigned char debounced;
+	GLFWwindow*    window;
+};
+
+struct KeyArray keyArray[512];
+
+int KeyDown(int key)
+{
+	return keyArray[key].curState==GLFW_PRESS;
+}
+
+int CheckKey(int key)
+{
+	return keyArray[key].debounced;
+}
+
+int CheckKeyWindow(int key,GLFWwindow* window)
+{
+	return keyArray[key].debounced && (keyArray[key].window==window);
+}
+
+void ClearKey(int key)
+{
+	keyArray[key].debounced=0;
+}
+
+void kbHandler( GLFWwindow* window, int key, int scan, int action, int mod )
+{
+	keyArray[key].lastState=keyArray[key].curState;
+	if (action==GLFW_RELEASE)
+	{
+		keyArray[key].curState=GLFW_RELEASE;
+	}
+	else
+	{
+		keyArray[key].curState=GLFW_PRESS;
+	}
+	keyArray[key].debounced|=(keyArray[key].lastState==GLFW_RELEASE)&&(keyArray[key].curState==GLFW_PRESS);
+	keyArray[key].window=window;
+}
+
+void sizeHandler(GLFWwindow* window,int xs,int ys)
+{
+	glfwMakeContextCurrent(window);
+	glViewport(0, 0, xs, ys);
+}
+
+void UpdateHardware()
+{
+}
+		
+int stopTheClock=0;
+
+// NTSC
+// Main Clock = 3.58Mhz
+// CPU Clock = 1.19Mhz		(Main/3)
+
+// TIA has 228 clocks per line	* 262 * 60 = ~3.58
+//
+// 
+int main(int argc,char**argv)
+{
+	int w,h;
+	double	atStart,now,remain;
+	uint16_t bp;
+
+	/// Initialize GLFW 
+	glfwInit(); 
+
+	// Open screen OpenGL window 
+	if( !(windows[MAIN_WINDOW]=glfwCreateWindow( WIDTH, HEIGHT, "2600",NULL,NULL)) ) 
+	{ 
+		glfwTerminate(); 
+		return 1; 
+	} 
+
+	glfwSetWindowPos(windows[MAIN_WINDOW],300,300);
+	
+	glfwMakeContextCurrent(windows[MAIN_WINDOW]);
+	setupGL(MAIN_WINDOW,WIDTH,HEIGHT);
+
+	glfwSwapInterval(0);			// Disable VSYNC
+
+	glfwGetWindowSize(windows[MAIN_WINDOW],&w,&h);
+
+	printf("width : %d (%d) , height : %d (%d)\n", w,WIDTH,h,HEIGHT);
+	glfwSetKeyCallback(windows[MAIN_WINDOW],kbHandler);
+	glfwSetWindowSizeCallback(windows[MAIN_WINDOW],sizeHandler);
+
+	atStart=glfwGetTime();
+	//////////////////
+	AudioInitialise();
+
+	if (InitialiseMemory())
+		return -1;
+
+	//dumpInstruction=100000;
+
+	MAIN_PinSet_RES(0);		// Trip reset
+	MAIN_PinSet_RES(1);
+	
+	while (!glfwGetKey(windows[MAIN_WINDOW],GLFW_KEY_ESCAPE))
+	{
+		static uint16_t lastPC;
+		static uint8_t lastPb0=0xff;
+		uint16_t addr; 
+		
+		if ((!stopTheClock) || g_traceStep || g_instructionStep)
+		{
+			MAIN_PinSetO0(1);
+			addr = MAIN_PinGetAB();
+
+			if (MAIN_PinGetSYNC())
+			{
+				lastPC=addr;
+
+				if (doDebug)
+				{
+					Disassemble(addr,1);
+					//getch();
+				}
+			}
+
+			if (MAIN_PinGetRW())
+			{
+				uint8_t  data = GetByte(addr);
+				if (doDebug)
+					printf("Getting : %02X @ %04X\n", data,addr);
+				MAIN_PinSetDB(data);
+			}
+			if (!MAIN_PinGetRW())
+			{
+				if (doDebug)
+					printf("Storing : %02X @ %04X\n", MAIN_PinGetDB(),addr);
+				SetByte(addr,MAIN_PinGetDB());
+			}
+
+			UpdateHardware();
+
+			MAIN_PinSetO0(0);
+
+			pixelClock++;
+		}
+
+		if (pixelClock>=22152 || stopTheClock)
+		{
+			static int normalSpeed=1;
+
+			if (pixelClock>=22152)
+				pixelClock-=22152;
+
+            		glfwMakeContextCurrent(windows[MAIN_WINDOW]);
+			ShowScreen(MAIN_WINDOW,WIDTH,HEIGHT);
+			glfwSwapBuffers(windows[MAIN_WINDOW]);
+				
+			glfwPollEvents();
+			
+			if (CheckKey(GLFW_KEY_INSERT))
+			{
+				ClearKey(GLFW_KEY_INSERT);
+				normalSpeed^=1;
+			}
+			if (CheckKey(GLFW_KEY_DELETE))
+			{
+				ClearKey(GLFW_KEY_DELETE);
+				stopTheClock^=1;
+			}
+			g_traceStep=0;
+			if (KeyDown(GLFW_KEY_KP_DIVIDE))
+			{
+				g_traceStep=1;
+			}
+			if (CheckKey(GLFW_KEY_KP_MULTIPLY))
+			{
+				ClearKey(GLFW_KEY_KP_MULTIPLY);
+				g_traceStep=1;
+			}
+			
+			now=glfwGetTime();
+
+			remain = now-atStart;
+
+			while ((remain<0.02f) && normalSpeed && !stopTheClock)
+			{
+				now=glfwGetTime();
+
+				remain = now-atStart;
+			}
+			atStart=glfwGetTime();
+		}
+	}
+
+	AudioKill();
+
+	return 0;
+
+}
+
+//////////////////////// NOISES //////////////////////////
+
+#define NUMBUFFERS            (2)				/* living dangerously*/
+
+ALuint		  uiBuffers[NUMBUFFERS];
+ALuint		  uiSource;
+
+ALboolean ALFWInitOpenAL()
+{
+	ALCcontext *pContext = NULL;
+	ALCdevice *pDevice = NULL;
+	ALboolean bReturn = AL_FALSE;
+	
+	pDevice = alcOpenDevice(NULL);				/* Request default device*/
+	if (pDevice)
+	{
+		pContext = alcCreateContext(pDevice, NULL);
+		if (pContext)
+		{
+			printf("\nOpened %s Device\n", alcGetString(pDevice, ALC_DEVICE_SPECIFIER));
+			alcMakeContextCurrent(pContext);
+			bReturn = AL_TRUE;
+		}
+		else
+		{
+			alcCloseDevice(pDevice);
+		}
+	}
+
+	return bReturn;
+}
+
+ALboolean ALFWShutdownOpenAL()
+{
+	ALCcontext *pContext;
+	ALCdevice *pDevice;
+
+	pContext = alcGetCurrentContext();
+	pDevice = alcGetContextsDevice(pContext);
+	
+	alcMakeContextCurrent(NULL);
+	alcDestroyContext(pContext);
+	alcCloseDevice(pDevice);
+
+	return AL_TRUE;
+}
+
+#if 0/*USE_8BIT_OUTPUT*/
+
+#define AL_FORMAT						(AL_FORMAT_MONO8)
+#define BUFFER_FORMAT				U8
+#define BUFFER_FORMAT_SIZE	(1)
+#define BUFFER_FORMAT_SHIFT	(8)
+
+#else
+
+#define AL_FORMAT						(AL_FORMAT_MONO16)
+#define BUFFER_FORMAT				int16_t
+#define BUFFER_FORMAT_SIZE	(2)
+#define BUFFER_FORMAT_SHIFT	(0)
+
+#endif
+
+int curPlayBuffer=0;
+
+#define BUFFER_LEN		(44100/50)
+
+BUFFER_FORMAT audioBuffer[BUFFER_LEN];
+int amountAdded=0;
+
+void AudioInitialise()
+{
+	int a=0;
+	for (a=0;a<BUFFER_LEN;a++)
+	{
+		audioBuffer[a]=0;
+	}
+
+	ALFWInitOpenAL();
+
+  /* Generate some AL Buffers for streaming */
+	alGenBuffers( NUMBUFFERS, uiBuffers );
+
+	/* Generate a Source to playback the Buffers */
+  alGenSources( 1, &uiSource );
+
+	for (a=0;a<NUMBUFFERS;a++)
+	{
+		alBufferData(uiBuffers[a], AL_FORMAT, audioBuffer, BUFFER_LEN*BUFFER_FORMAT_SIZE, 44100);
+		alSourceQueueBuffers(uiSource, 1, &uiBuffers[a]);
+	}
+
+	alSourcePlay(uiSource);
+}
+
+
+void AudioKill()
+{
+	ALFWShutdownOpenAL();
+}
+
+int16_t currentDAC[4] = {0,0,0,0};
+
+void _AudioAddData(int channel,int16_t dacValue)
+{
+	currentDAC[channel]=dacValue;
+}
+
+uint32_t tickCnt=0;
+uint32_t tickRate=((22152*4096)/(44100/50));
+
+/* audio ticked at same clock as everything else... so need a step down */
+void UpdateAudio()
+{
+	tickCnt+=1*4096;
+	
+	if (tickCnt>=tickRate*50)
+	{
+		tickCnt-=tickRate;
+
+		if (amountAdded!=BUFFER_LEN)
+		{
+			int32_t res=0;
+
+			res+=currentDAC[0];
+			res+=currentDAC[1];
+			res+=currentDAC[2];
+			res+=currentDAC[3];
+
+			audioBuffer[amountAdded]=res>>BUFFER_FORMAT_SHIFT;
+			amountAdded++;
+		}
+	}
+
+	if (amountAdded==BUFFER_LEN)
+	{
+		/* 1 second has passed by */
+
+		ALint processed;
+		ALint state;
+		alGetSourcei(uiSource, AL_SOURCE_STATE, &state);
+		alGetSourcei(uiSource, AL_BUFFERS_PROCESSED, &processed);
+		if (processed>0)
+		{
+			ALuint buffer;
+
+			amountAdded=0;
+			alSourceUnqueueBuffers(uiSource,1, &buffer);
+			alBufferData(buffer, AL_FORMAT, audioBuffer, BUFFER_LEN*BUFFER_FORMAT_SIZE, 44100);
+			alSourceQueueBuffers(uiSource, 1, &buffer);
+		}
+
+		if (state!=AL_PLAYING)
+		{
+			alSourcePlay(uiSource);
+		}
+	}
+}
+
+
